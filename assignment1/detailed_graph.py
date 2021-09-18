2fcac73f7a8a96827f1b1a2c007db7c159ad3021
abishekshivram <abishekshivram@gmail.com>
2021-09-18 12:42:01 +0530

    l1 and l2 inconsistency assertion

--- assignment1/detailed_graph.py
+++ assignment1/detailed_graph.py
@@ -7,43 +7,57 @@ from foldedTorus import FoldedTorus
 
 def network(level,networkType,idx,n,m):
 	networkID = level+"_"+networkType+str(idx)+"_"
+	total_head_nodes = 0
 	
 	if (networkType == 'B'):
 		assert (n==m),"Dimensions of Butterfly network should be equal"
 		network = Butterfly(networkID,n,False if level == 'L1' else True)
+		if level == 'L1':
+			total_head_nodes = 2*m
 	
 	elif (networkType == 'C'):
 		assert (m==1),"Second dimension of Chain network is invalid"
 		network = Chain(networkID,n,False if level == 'L1' else True)
+		if level == 'L1':
+			total_head_nodes = n
 
 	elif (networkType == 'R'):
 		assert (m==1),"Second dimension of Ring network is invalid"
 		network = Ring(networkID,n,False if level == 'L1' else True)
+		if level == 'L1':
+			total_head_nodes = n
 
 	elif (networkType == 'M'):
 		assert (m>1 and n>1 and m+n>4),"The given dimension doesn't correspond to a Mesh network"
 		network = Mesh(networkID,m,n,False if level == 'L1' else True)
+		if level == 'L1':
+			total_head_nodes = m*n
 
 	elif (networkType == 'F'):
 		assert (m>1 and n>1 and m+n>4),"The given dimension doesn't correspond to a Folded Torus network"
 		network = FoldedTorus(networkID,m,n,False if level == 'L1' else True)
+		if level == 'L1':
+			total_head_nodes = m*n
 
 	elif (networkType == 'H'):
 		assert (m==n),"Please specify the dimension of the Hypercube network properly"
 		network = Hypercube(networkID,n,False if level == 'L1' else True)
+		if level == 'L1':
+			total_head_nodes = 2**n
 
 	else:
 		print("Please choose a valid network")
 		network = None
 	
-	return network
+	return network, total_head_nodes
 
 # Reading L1 topology text file
 # This file contains how all the central nodes of each network are connected
 with open ("L1Topology.txt",'r') as f:
 	for line in f:
 		L1 = line.strip().replace(" ", "").split(',')
-		L1_network = network('L1',L1[0],0,int(L1[1]),int(L1[2]))
+		L1_network, total_head_nodes = network('L1',L1[0],0,int(L1[1]),int(L1[2]))
+
 
 # Reading L2 topology text file
 # This contains details of each of the network
@@ -55,8 +69,11 @@ with open ("L2Topology.txt", 'r') as f:
 		if (line=='\n'):
 			continue
 		L2.append(line.strip().replace(" ", "").split(','))
-		L2_network.append(network('L2',L2[-1][0],idx,int(L2[-1][1]),int(L2[-1][2])))
+		L2_network.append(network('L2',L2[-1][0],idx,int(L2[-1][1]),int(L2[-1][2]))[0])
 		idx += 1
+
+assert (len(L2_network) == total_head_nodes), "Please check your inputs, L1 and L2 inconsistency in number of nodes"
+
 
 for i in L2_network:
 	L1_network.insert_nodes(i.get_head_node())
