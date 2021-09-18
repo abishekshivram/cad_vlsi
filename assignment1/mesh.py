8f360c88d88f2c5b4848c6dc8f68b6f1cf5099b2
Lloyd <cs21m001@smail.iitm.ac.in>
2021-09-18 15:22:55 +0530

    Added documentation

--- assignment1/mesh.py
+++ assignment1/mesh.py
@@ -1,8 +1,40 @@
+###########################################################################
+# CS6230:CAD for VLSI Systems - Project 1
+# Name: Two level hierarchical Network on Chip 
+# Team Name: Kilbees
+# Team Members: Abishekshivram AM (EE18B002)
+#               Gayatri Ramanathan Ratnam (EE18B006)
+#               Lloyd K L (CS21M001)
+# Description: Implementation of the Mesh network topology
+# Last updated on: 18-Sep-2021
+############################################################################
+
 from node import Node
 from butterfly import dec_to_bin,log
 
 class Mesh:
+    """
+    A class to represent the Mesh based network topology.
+    A node in the network will have three or four neighbours except head node
+    If networks are interconnected, head node can have more than four neighbours
+    """
+
     def __init__(self,name,rowCount,colCount,create=True):
+        """
+        Initialises the Mesh class.
+
+        Parameters:
+        -----------
+        name : string
+            The name to be assigned to the network topology.
+        Count : Integer
+            The number of nodes to be added in the topology
+        Ceate : True/False
+            If true, the nodes of the topology are created
+            If false, the internal state is updated. Nodes are not created
+            Default is True
+        """
+
         self.name=name
         self.rowCount=rowCount
         self.colCount=colCount
@@ -11,10 +43,28 @@ class Mesh:
             self.create_nodes()
 
     def get_node_name(self,i,j):
+        """
+        Generates a node name based on the topology and the Id given
+        Appends the given Id to the topology name to generate a new node name
+
+        Parameters:
+        -----------
+        id : Integer
+            The idd to be added to the node name
+
+        Returns:
+        --------
+        The name generated for the node
+        """
+
         return self.name+str(i)+"_"+str(j)
 
     def create_nodes(self):
-        #init
+        """
+        A private function to initialise the nodes in the topology
+        This function, creates and assigns unique name to each node
+        """
+
         row_bits = log(self.rowCount)
         col_bits = log(self.colCount)
         for i in range(0,self.rowCount):
@@ -25,6 +75,18 @@ class Mesh:
         self.create_network()
         
     def insert_nodes(self,node):
+        """
+        Insert a new node to the topology.
+        This function is used to add a new node to the topology,
+        if the number of nodes is less than what is specfied at the time of topology creation
+        This function is used to add Layer 2 topology head node in to the Layer 1 network
+
+        Parameters:
+        -----------
+        node : An object of Node class
+            The new node to be added to the topology
+        """
+
         max_row = len(self.rowVertices)
         
         if max_row > 0:
@@ -40,6 +102,9 @@ class Mesh:
         self.rowVertices[-1].append(node)
         
     def create_network(self):
+        """
+        Connects the nodes in the topology to form the mesh network
+        """
         #Nodes in the center
         for i in range(1,len(self.rowVertices)-1):
             for j in range(1,len(self.rowVertices[i])-1):
@@ -84,12 +149,27 @@ class Mesh:
             
             
     def print_nodes(self):
+        """
+        Prints all the nodes in the network.
+        Calls the print_neighbour() function in node class
+        Prints nodes as per the output file format specification in the problem statement
+        """
+
         for i in range(0,self.rowCount):
             for j in range(0,self.colCount):
                 self.rowVertices[i][j].print_neighbour()
 
 
     def get_head_node(self):
+        """
+        Calculates the head node of the topology.
+        In the case of Mesh, it is the node in center
+
+        Returns:
+        --------
+        The head node of the topology. Empty node, if the topology is not created
+        """
+
         midrow=self.rowCount//2
         midcol=self.colCount//2
 
