872b364aa93af6ff3af14b8c45ceef7f5549bb15
Gayatri RR <ee18b006@smail.iitm.ac.in>
2021-09-26 22:56:48 +0530

    Adding half built main function for verification pt 2

--- assignment2/verification.py
+++ assignment2/verification.py
@@ -27,6 +27,33 @@ rx_dict = {
     'NeighbourLink': compile(r'^L(?P<L_idx>\d+): (?P<neighbourID>.*)\n')
 }
 
+
+def nodeIDInterpreter(nodeID):
+    lenNodeID = len(nodeID)
+    assert lenNodeID in [4,5],"Please check your nodeID specification"
+    level= nodeID[0]
+    lvl2networkNum = nodeID[1]
+    lvl2networkType = nodeID[2]
+    assert lvl2networkType in ['C','B','F','R','M','H'],"Network Type Specified should be one of 'C','B','F','R','M' or 'H'"
+    
+    if lvl2networkType == 'B':
+        if (lenNodeID == 4):
+            assert nodeID[3][0] in ['L','R'], "Butterfly end nodes can only be of the form 'L' or 'R'"
+            if nodeID[3][0] == 'L':
+                butterflySide = 'L'
+                nodeIndex = int(nodeID[3][1:])
+            else:
+               butterflySide = 'R'
+               nodeIndex = int(nodeID[3][1:]) 
+        else:
+            assert nodeID[3][0] == 'S', "Switches of a butterfly network should be denoted with 'S'"
+            switchNum = int(nodeID[3][1:])   
+            switchNodeIndex = int(nodeID[4]) 
+    else:
+        nodeIndex = int(nodeID[3])
+
+
+# MAIN function equivalent part of the program
 with open (FILE_PATH,'r') as f:
     for line in f:
         if (line=='\n'):
@@ -36,8 +63,12 @@ with open (FILE_PATH,'r') as f:
         
         if key == 'NodeID':
             nodeID = match.group('nodeID').strip().split('_')
+            nodeIDInterpreter(nodeID)
+
         elif key == 'NumLink':
             numLinks = int(match.group('numLinks').strip())
         elif key == 'NeighbourLink':
             neighbourNum = int(match.group('L_idx').strip())
             neighbourID = match.group('neighbourID').strip().split('_')
+            nodeIDInterpreter(nodeID)
+
