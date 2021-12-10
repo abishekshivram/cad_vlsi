4f7d8f6e697898136327f1dc92cb2d1eba3287e7
Abishekshivram <ee18b002@smail.iitm.ac.in>
2021-12-10 15:15:09 +0530

    Butterfly and Hypercube other files modified

--- final_project/VC/Butterfly/butterfly_printer.py
+++ final_project/VC/Butterfly/butterfly_printer.py
@@ -11,24 +11,24 @@ switch_connect = """    rule add_to_link_right0(counter == 2'b00);
 """
 
 node_instantiate = """    Address node{id}_address;  node{id}_address.netAddress=0;  node{id}_address.nodeAddress={id};
-    let node{id:0>4b} <- mkButterflyNode(node{id}_address);"""
+    let node{id:0>5b} <- mkButterflyNode(node{id}_address);"""
 
-switches_inst = """     let switch_L{layer}_{id} <- mkButterflySwitch(node{id}_address, {layer});"""
+switches_inst = """     let switch_L{layer}_{id} <- mkButterflySwitch(node{id}_address, {layer}, {left_ext}, {right_ext});"""
 
 left_nodes_to_switches = """    rule node{node_id}_to_switch_L{layer}_{id};
-        let data <- node{node_id:0>4b}.get_value();
+        let data <- node{node_id:0>5b}.get_value();
         switch_L{layer}_{id}.put_value_from_left_{up_down}(data);
     endrule
 """
 switches_to_left_nodes = """   rule to_switch_L{layer}_{id}_node{node_id};
         let data <- switch_L{layer}_{id}.get_value_to_left_{up_down}();
-        node{node_id:0>4b}.put_value();
+        node{node_id:0>5b}.put_value();
     endrule
 """
 
 
 right_nodes_to_switches = """    rule node{node_id}_to_switch_L{layer}_{id};
-        let data <- node{node_id:0>4b}.get_value();
+        let data <- node{node_id:0>5b}.get_value();
         switch_L{layer}_{id}.put_value_from_right_{up_down}(data);
     endrule
 """
@@ -36,24 +36,26 @@ right_nodes_to_switches = """    rule node{node_id}_to_switch_L{layer}_{id};
 
 switches_to_right_nodes = """    rule to_switch_L{layer}_{id}_node{node_id};
         let data <- switch_L{layer}_{id}.get_value_to_right_{up_down}();
-        node{node_id:0>4b}.put_value();
+        node{node_id:0>5b}.put_value();
     endrule
 """
 
 up_down = "down"
 
-for i in range(16):
+for i in range(32):
     # print(node_instantiate.format(id=i))
-    # if(i<12):
-        # print(switches_inst.format(layer=i//4, id=i%4))
-    if(i<8):
+    if(i<8*4):
+        left_ext = "True" if i < 8 else "False"
+        right_ext = "True" if i >= 3*8 else "False" 
+        # print(switches_inst.format(layer=i//8, id=i%8, left_ext=left_ext, right_ext=right_ext))
+    if(i<16):
         up_down = "down" if up_down=="up" else "up"
-        # print(left_nodes_to_switches.format(layer=(i//2)//4, id=(i//2)%4, node_id=i, up_down=up_down))
-        print(switches_to_left_nodes.format(layer=(i//2)//4, id=(i//2)%4, node_id=i, up_down=up_down))
+        # print(left_nodes_to_switches.format(layer=(i//2)//8, id=(i//2)%8, node_id=i, up_down=up_down))
+        # print(switches_to_left_nodes.format(layer=(i//2)//8, id=(i//2)%8, node_id=i, up_down=up_down))
     else:
         up_down = "down" if up_down=="up" else "up"
-        # print(right_nodes_to_switches.format(layer=1+(i//2)//4, id=(i//2)%4, node_id=i, up_down=up_down))
-        # print(switches_to_right_nodes.format(layer=1+(i//2)//4, id=(i//2)%4, node_id=i, up_down=up_down))
+        # print(right_nodes_to_switches.format(layer=2+(i//2)//8, id=(i//2)%8, node_id=i, up_down=up_down))
+        # print(switches_to_right_nodes.format(layer=2+(i//2)//8, id=(i//2)%8, node_id=i, up_down=up_down))
 
     
 # CONNECT SWITCHES (UP==STRAIGHT) (DOWN==CROSS)
@@ -80,8 +82,8 @@ switch_connect_l2r_cross = """    rule connect_l2r_switchL{layer_from}_{row_from
 """
 # switch_connect_r2l
 
-SIZE= 8 # 8x8
-for layer in range(2):
+SIZE= 16 # 16x16
+for layer in range(3):
     for row in range(SIZE//2):
         keys = {}
         keys['layer_from'] = layer
@@ -90,9 +92,9 @@ for layer in range(2):
         # print(switch_connect_l2r_straight.format(layer_from=layer, layer_to=layer+1,row_from=row, row_to=row))
         # print(switch_connect_l2r_straight.format(**keys))
 
-MAX_OFFSET = 2*2
-MAX_ROWS = 4*2
-for layer in range(2):
+MAX_OFFSET = 4*2
+MAX_ROWS = 8*2
+for layer in range(3):
     print(f"// LAYER {layer}")
     MAX_OFFSET = MAX_OFFSET//2
     MAX_ROWS = MAX_ROWS//2
@@ -101,5 +103,4 @@ for layer in range(2):
         keys['layer_from'] = layer
         keys['layer_to'] = layer+1
         keys['row_from'], keys['row_to'] = row, (row+MAX_OFFSET)%MAX_ROWS + (row//MAX_ROWS)*MAX_ROWS
-        # print(switch_connect_l2r_straight.format(layer_from=layer, layer_to=layer+1,row_from=row, row_to=row))
-        # print(switch_connect_l2r_cross.format(**keys))
+        print(switch_connect_l2r_cross.format(**keys))
